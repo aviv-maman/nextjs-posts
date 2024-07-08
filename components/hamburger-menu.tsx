c39357e919a6fca7e361aee8d2fbe916cd7398be
Aviv Maman <awiwmaman@outlook.com>
2024-07-08 14:30:06 +0300

    Update hamburger-menu.tsx

--- components/hamburger-menu.tsx
+++ components/hamburger-menu.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { HomeIcon, MenuIcon } from 'lucide-react';
+import { Home, Menu } from 'lucide-react';
 import Link from 'next/link';
-import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from './ui/sheet';
 
 interface HamburgerMenuProps {
   components?: { title: string; href: string; description: string }[];
@@ -12,14 +12,17 @@ export const HamburgerMenu: React.FC<HamburgerMenuProps> = ({ components }) => {
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <MenuIcon className='size-5 hover:cursor-pointer hover:text-indigo-300' />
+        <Menu className='size-5 hover:cursor-pointer hover:text-indigo-300' />
       </SheetTrigger>
       <SheetContent side='left'>
+        <SheetHeader className='items-start'>
+          <SheetTitle className='flex items-center gap-x-2'>
+            <Home className='size-5' />
+            <span className='font-bold'>ToDoz</span>
+          </SheetTitle>
+          <SheetDescription className='sr-only'>Choose a page you would like to navigate to.</SheetDescription>
+        </SheetHeader>
         <div className='relative h-[calc(100vh-8rem)] overflow-hidden pb-10'>
-          <div className='flex gap-x-2'>
-            <HomeIcon className='size-5' />
-            <div className='font-bold'>ToDoz</div>
-          </div>
           <div className='flex flex-col space-y-2 pl-6'>
             <span className='mt-2 w-fit text-left'>Menu</span>
             {components?.map((component) => (
