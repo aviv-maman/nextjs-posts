55854a5b8cebe36d3025f95ee7c74cfd4823a093
Aviv Maman <awiwmaman@outlook.com>
2024-07-11 00:12:47 +0300

    site color and theme-toggle change

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Moon, Sun } from 'lucide-react';
+import { Computer, Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 import { Button } from '@/components/ui/button';
 import {
@@ -13,19 +13,33 @@ import {
 export function ThemeToggle() {
   const { setTheme } = useTheme();
 
+  const items = [
+    { label: 'Light', icon: <Sun className='size-4' /> },
+    { label: 'Dark', icon: <Moon className='size-4' /> },
+    { label: 'System', icon: <Computer className='size-4' /> },
+  ];
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant='ghost' size='icon' className='size-9 bg-transparent'>
-          <Sun className='size-5 rotate-0 scale-100 text-yellow-500 transition-all dark:-rotate-90 dark:scale-0' />
-          <Moon className='absolute size-5 rotate-90 scale-0 text-yellow-400 transition-all dark:rotate-0 dark:scale-100' />
+          <Sun className='size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+          <Moon className='absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
           <span className='sr-only'>Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align='end'>
-        <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
+        {items.map((item) => {
+          return (
+            <DropdownMenuItem
+              key={item.label}
+              className='cursor-pointer justify-between'
+              onClick={() => setTheme(item.label.toLowerCase())}>
+              <span>{item.label}</span>
+              {item.icon}
+            </DropdownMenuItem>
+          );
+        })}
       </DropdownMenuContent>
     </DropdownMenu>
   );
