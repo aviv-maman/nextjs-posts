b0916229db8d91c1c5373da26697c89d579aa626
Aviv Maman <awiwmaman@outlook.com>
2024-07-10 20:50:24 +0300

    CSS - 2

--- components/examples/SliderDemo.tsx
+++ components/examples/SliderDemo.tsx
@@ -4,5 +4,9 @@ import { cn } from '@/lib/utils';
 type SliderProps = React.ComponentProps<typeof Slider>;
 
 export function SliderDemo({ className, ...props }: SliderProps) {
-  return <Slider defaultValue={[50]} max={100} step={1} className={cn('w-[60%]', className)} {...props} />;
+  return (
+    <section className='w-full sm:w-[384px]'>
+      <Slider defaultValue={[50]} max={100} step={1} className={cn('w-full', className)} {...props} />
+    </section>
+  );
 }
--- components/examples/TextareaDemo.tsx
+++ components/examples/TextareaDemo.tsx
@@ -4,11 +4,11 @@ import { Textarea } from '@/components/ui/textarea';
 
 export function TextareaDemo() {
   return (
-    <div className='grid w-full gap-1.5'>
+    <section className='grid w-full gap-1.5 sm:w-[384px]'>
       <Label htmlFor='message'>Your message</Label>
       <Textarea placeholder='Type your message here.' id='message' className='resize-none' />
       <p className='text-sm text-muted-foreground'>Your message will be copied to the support team.</p>
       <Button>Send message</Button>
-    </div>
+    </section>
   );
 }
