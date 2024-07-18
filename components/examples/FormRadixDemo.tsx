e9250ce699fc586b9e254898fdb8369a5aa84611
Aviv Maman <awiwmaman@outlook.com>
2024-07-18 15:14:54 +0300

    Update FormRadixDemo.tsx

--- components/examples/FormRadixDemo.tsx
+++ components/examples/FormRadixDemo.tsx
@@ -33,22 +33,14 @@ const formSchema = z.object({
 type FlattenedErrors = z.inferFlattenedErrors<typeof formSchema>;
 
 export function FormRadixDemo() {
-  const [zodErrors, setZodErrors] = useState<{
-    fieldErrors?: FlattenedErrors['fieldErrors'];
-    formError?: string;
-    serverErrors?: FlattenedErrors['fieldErrors'] & { general?: string };
-  }>({});
+  const [clientErrors, setClientErrors] = useState<FlattenedErrors>();
 
   const customEmailValidation = (value: string, formData: FormData) => {
     const rawFormData = Object.fromEntries(formData);
     const errors = formSchema.safeParse(rawFormData).error?.flatten();
 
     if (errors?.fieldErrors.email) {
-      setZodErrors((prevState) => ({
-        ...prevState,
-        formError: errors.formErrors?.[0],
-        fieldErrors: { email: errors.fieldErrors.email },
-      }));
+      setClientErrors(() => errors);
       return true;
     }
     return false;
@@ -59,11 +51,7 @@ export function FormRadixDemo() {
     const errors = formSchema.safeParse(rawFormData).error?.flatten();
 
     if (errors?.fieldErrors.username) {
-      setZodErrors((prevState) => ({
-        ...prevState,
-        formError: errors.formErrors?.[0],
-        fieldErrors: { username: errors.fieldErrors.username },
-      }));
+      setClientErrors(() => errors);
       return true;
     }
     return false;
@@ -74,39 +62,29 @@ export function FormRadixDemo() {
     const errors = formSchema.safeParse(rawFormData).error?.flatten();
 
     if (errors?.fieldErrors.password) {
-      setZodErrors((prevState) => ({
-        ...prevState,
-        formError: errors.formErrors?.[0],
-        fieldErrors: { password: errors.fieldErrors.password },
-      }));
+      setClientErrors(() => errors);
       return true;
     }
     return false;
   };
 
-  type FormState =
+  type ActionState =
     | {
-        actionErrors?: {
-          formErrors?: FlattenedErrors['fieldErrors'] & { general?: string[] };
-          serverErrors?: FlattenedErrors['fieldErrors'] & { general?: string[] };
-        };
+        errors?: FlattenedErrors['fieldErrors'] & { general?: string[] };
         result?: { success: boolean };
       }
     | undefined;
-  async function authenticate(prevState: FormState, formData: FormData): Promise<FormState> {
+
+  async function authenticate(prevState: ActionState, formData: FormData): Promise<ActionState> {
     const rawFormData = Object.fromEntries(formData);
     const validatedFields = formSchema.safeParse(rawFormData);
     if (!validatedFields.success) {
-      return {
-        actionErrors: { formErrors: { general: validatedFields.error.flatten().formErrors } },
-      };
+      return { errors: { general: validatedFields.error.flatten().formErrors } };
     }
     if (validatedFields.data.email !== 'b@b.com') {
       console.log('email');
 
-      return {
-        actionErrors: { serverErrors: { email: ['Only b@b.com is allowed'], general: ['Internal Server Error'] } },
-      };
+      return { errors: { email: ['Only b@b.com is allowed'], general: ['Internal Server Error'] } };
     }
     try {
       //await signIn(validatedFields.data.provider, formData);
@@ -114,36 +92,30 @@ export function FormRadixDemo() {
       return { result: { success: true } };
     } catch (error: any) {
       return {
-        actionErrors: { serverErrors: { general: [String(error.message || 'Something went wrong')] } },
+        errors: { general: [String(error.message || 'Something went wrong')] },
       };
     }
   }
-  const [formState, formAction] = useFormState(authenticate, undefined);
+  const [actionState, formAction] = useFormState(authenticate, undefined);
 
   return (
     <section className='flex flex-col space-y-2'>
-      <FormRoot
-        className='space-y-4'
-        action={formAction}
-        onClearServerErrors={() =>
-          setZodErrors((prevState) => ({ ...prevState, formError: undefined, serverErrors: undefined }))
-        }>
+      <FormRoot className='space-y-4' action={formAction}>
         {/* Email Field */}
-        <FormField name='email' serverInvalid={Boolean(formState?.actionErrors?.serverErrors?.email)}>
+        <FormField name='email' serverInvalid={Boolean(actionState?.errors?.email)}>
           <FormLabel>Email</FormLabel>
           <FormControl>
-            <Input type='email' placeholder='Enter your email' />
+            <Input placeholder='Enter your email' />
           </FormControl>
           <FormDescription>Your email address</FormDescription>
-          <FormMessage match='typeMismatch'>Please provide a valid email.</FormMessage>
-          <FormMessage match={customEmailValidation}>{zodErrors.fieldErrors?.email?.[0]}</FormMessage>
-          {formState?.actionErrors?.serverErrors?.email?.[0] && (
-            <FormMessage>{formState?.actionErrors?.serverErrors?.email?.[0]}</FormMessage>
-          )}
+          <div className='flex flex-col'>
+            <FormMessage match={customEmailValidation}>{clientErrors?.fieldErrors?.email?.[0]}</FormMessage>
+            {actionState?.errors?.email?.[0] && <FormMessage>{actionState?.errors?.email?.[0]}</FormMessage>}
+          </div>
         </FormField>
 
         {/* Username Field */}
-        <FormField name='username' serverInvalid={Boolean(formState?.actionErrors?.serverErrors?.username)}>
+        <FormField name='username' serverInvalid={Boolean(actionState?.errors?.username)}>
           <FormLabel>Username</FormLabel>
           <FormValidityState>
             {(validity) => (
@@ -153,32 +125,30 @@ export function FormRadixDemo() {
             )}
           </FormValidityState>
           <FormDescription>Your username</FormDescription>
-          <FormMessage match='typeMismatch'>Please provide a valid username.</FormMessage>
-          <FormMessage match={customUsernameValidation}>{zodErrors?.fieldErrors?.username?.[0]}</FormMessage>
-          {formState?.actionErrors?.serverErrors?.username?.[0] && (
-            <FormMessage>{formState.actionErrors?.serverErrors.username[0]}</FormMessage>
-          )}
+          <div className='flex flex-col'>
+            <FormMessage match='typeMismatch'>typeMismatch: Please provide a valid username.</FormMessage>
+            <FormMessage match={customUsernameValidation}>{clientErrors?.fieldErrors?.username?.[0]}</FormMessage>
+          </div>
+          {actionState?.errors?.username?.[0] && <FormMessage>{actionState.errors.username[0]}</FormMessage>}
         </FormField>
 
         {/* Password Field */}
-        <FormField name='password' serverInvalid={Boolean(formState?.actionErrors?.serverErrors?.password)}>
+        <FormField name='password' serverInvalid={Boolean(actionState?.errors?.password)}>
           <FormLabel>Password</FormLabel>
           <FormControl>
             <Input type='password' placeholder='Enter your password' />
           </FormControl>
           <FormDescription>Your password</FormDescription>
-          <FormMessage match='typeMismatch'>Please provide a valid password.</FormMessage>
-          <FormMessage match={customPasswordValidation}>{zodErrors?.fieldErrors?.password?.[0]}</FormMessage>
-          {formState?.actionErrors?.serverErrors?.password?.[0] && (
-            <FormMessage>{formState.actionErrors?.serverErrors.password[0]}</FormMessage>
-          )}
+          <div className='flex flex-col'>
+            <FormMessage match='typeMismatch'>typeMismatch: Please provide a valid password.</FormMessage>
+            <FormMessage match={customPasswordValidation}>{clientErrors?.fieldErrors?.password?.[0]}</FormMessage>
+          </div>
+          {actionState?.errors?.password?.[0] && <FormMessage>{actionState.errors.password[0]}</FormMessage>}
         </FormField>
 
         {/* Form Error */}
         <FormDescription className='text-destructive'>
-          {zodErrors.formError ||
-            formState?.actionErrors?.formErrors?.general?.[0] ||
-            formState?.actionErrors?.serverErrors?.general?.[0]}
+          {clientErrors?.formErrors?.[0] || actionState?.errors?.general?.[0]}
         </FormDescription>
 
         {/* Submit Button */}
