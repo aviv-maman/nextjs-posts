636d2dd923bae6b35fd55c2996043bad7236e5e2
Aviv Maman <awiwmaman@outlook.com>
2024-07-24 16:23:15 +0300

    Update items-actions.ts

--- lib/items-actions.ts
+++ lib/items-actions.ts
@@ -1,13 +1,12 @@
 'use server';
 
 import { neon } from '@neondatabase/serverless';
-import { generateId } from 'lucia';
-import { validateRequest } from './auth';
+import { validateRequest } from '@/lib/auth';
 
 export type AddGenericItemState =
   | {
       errors?: { title?: string[]; content?: string[] } & { general?: string[] };
-      result?: { success: boolean };
+      result?: { success: boolean; id: string };
     }
   | undefined;
 
@@ -40,51 +39,17 @@ export const addGenericItem = async (
   try {
     const { user } = await validateRequest();
     if (!user?.id) return { errors: { general: ['No user'] } };
-    const itemId = generateId(15);
-
-    // db.prepare(
-    //   'INSERT INTO generic_item (id, title, content, is_published, is_private, images, tags, website, owner_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    // ).run(itemId, title, content, 1, 0, JSON.stringify(imageUrls), JSON.stringify(tags), website, user.id);
 
     const sql = neon(process.env.DATABASE_URL!);
 
-    // await sql`CREATE TABLE IF NOT EXISTS generic_item
-    // (id TEXT NOT NULL PRIMARY KEY,
-    // title TEXT NOT NULL,
-    // content TEXT NOT NULL,
-    // is_published INTEGER NOT NULL,
-    // is_private INTEGER NOT NULL,
-    // images JSONB NOT NULL,
-    // tags JSONB NOT NULL,
-    // website VARCHAR(255),
-    // created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    // updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    // owner_id TEXT NOT NULL,
-    // FOREIGN KEY (owner_id) REFERENCES user(id)
-    // )`;
-
-    await sql`CREATE TABLE IF NOT EXISTS generic_item
-              (id TEXT NOT NULL PRIMARY KEY,
-              title TEXT NOT NULL,
-              content TEXT NOT NULL,
-              is_published INTEGER NOT NULL,
-              is_private INTEGER NOT NULL,
-              images JSONB NOT NULL,
-              tags JSONB NOT NULL,
-              website VARCHAR(255),
-              created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-              updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-              owner_id TEXT NOT NULL
-              )`;
-    await sql(
-      `
-      INSERT INTO generic_item (id, title, content, is_published, is_private, images, tags, website, owner_id)
-      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-      `,
-      [itemId, title, content, 1, 0, JSON.stringify(imageUrls), JSON.stringify(tags), website, user.id],
+    const record = await sql(
+      `INSERT INTO generic_item (title, content, is_published, is_private, images, tags, website, owner_id)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+      RETURNING id`,
+      [title, content, 1, 0, JSON.stringify(imageUrls), JSON.stringify(tags), website, user.id],
     );
 
-    return { result: { success: true } };
+    return { result: { success: true, id: record[0]?.id } };
   } catch (error) {
     const err = error instanceof Error ? error.message : 'Something went wrong';
     return {
