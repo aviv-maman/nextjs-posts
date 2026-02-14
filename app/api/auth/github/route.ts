080bb9716416a74d94e8915d5c1a7e9ae2f695cc
Aviv Maman <awiwmaman@outlook.com>
2026-02-14 11:34:06 +0200

    fix(set-cookies): fix cookies

--- app/api/auth/github/route.ts
+++ app/api/auth/github/route.ts
@@ -6,7 +6,7 @@ export async function GET(): Promise<Response> {
   const state = generateState();
   const url = await github.createAuthorizationURL(state, { scopes: ['read:user', 'user:email'] });
 
-  cookies().set('github_oauth_state', state, {
+  (await cookies()).set('github_oauth_state', state, {
     path: '/',
     secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
